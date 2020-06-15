fix bug with calculation of number of trials IPSI CONTRA

papermill_and_helper_functions/Notebook_7_helper_functions.py
@@ -23,16 +23,17 @@ def extract_data_of_interest_ipsi_contra_per_session (PhotoData_perTrial_channel
     
 def calculate_mean_sem(data_ipsi_contra):
     
-    number_of_trials = len(data_ipsi_contra[0].columns)
+    number_of_trials_ipsi = len(data_ipsi_contra[0].columns)
+    number_of_trials_contra = len(data_ipsi_contra[1].columns)
     
     mean_ipsi_next = data_ipsi_contra[0].mean(axis=1)
     mean_contra_next = data_ipsi_contra[1].mean(axis=1)
     
-    sem_ipsi_next = data_ipsi_contra[0].std(axis=1)/(number_of_trials**0.5)
-    sem_contra_next = data_ipsi_contra[1].std(axis=1)/(number_of_trials**0.5)
+    sem_ipsi_next = data_ipsi_contra[0].std(axis=1)/(number_of_trials_ipsi**0.5)
+    sem_contra_next = data_ipsi_contra[1].std(axis=1)/(number_of_trials_contra**0.5)
     
     
-    return (mean_ipsi_next,sem_ipsi_next,mean_contra_next,sem_contra_next,number_of_trials)
+    return (mean_ipsi_next,sem_ipsi_next,mean_contra_next,sem_contra_next,number_of_trials_ipsi,number_of_trials_contra)
 
 
 """
@@ -54,7 +55,7 @@ def plot_ipsi_contra_together (data_ipsi_contra_mean_sem,sem_traces,
                                trial_type,cell_type,y_axis,path_to_plot):
 
     plt.plot(data_ipsi_contra_mean_sem[0],linewidth=2, label="ipsi_next"+" "+"("+str(data_ipsi_contra_mean_sem[4])+")")
-    plt.plot(data_ipsi_contra_mean_sem[2],linewidth=2, label="contra_next"+" "+"("+str(data_ipsi_contra_mean_sem[4])+")")
+    plt.plot(data_ipsi_contra_mean_sem[2],linewidth=2, label="contra_next"+" "+"("+str(data_ipsi_contra_mean_sem[5])+")")
     
     plt.plot(sem_traces[0],color='black', linewidth=0.5,alpha=0.8)
     plt.plot(sem_traces[1],color='black', linewidth=0.5,alpha=0.8)