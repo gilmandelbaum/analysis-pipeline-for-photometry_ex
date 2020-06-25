00a2675196fe123137078f06a3ac59ed1c58c2f2
Gil Mandelbaum <gilmandelbaum@Gils-MBP.cable.rcn.com>
2020-06-25 01:06:33 -0400

    working on data set analysis

--- Nb_6x_combine_periods_of_interest/Notebook_6_helper_functions.py
+++ Nb_6x_combine_periods_of_interest/Notebook_6_helper_functions.py
@@ -28,3 +28,27 @@ def combine_periods_1(photo_data):
                     pass
                     print ("no new key")
     return photo_data
+
+
+
+
+
+def combine_periods_2(photo_data):
+#Combines: -1secPreConsumption + Consumption + last_sec_ENL + Cue
+    for rl in photo_data:
+        for ic in rl:
+            for combo_dict in ic:
+                try:
+                    #Periods:
+                    pre_Cue_to_Sel = combo_dict['pre_Cue_to_Sel_tag']
+                    Cue_to_Sel = combo_dict['Cue_to_Sel_tag']
+                    Reward_NoReward = combo_dict['Reward_NoReward_tag'] #should i make this more general? how can i do it?
+                    #first_bins_Cue_to_Sel = combo_dict['first_15_bins_Cue_to_Sel']
+                    combined_periods = pd.concat([pre_Cue_to_Sel, Cue_to_Sel , Reward_NoReward])
+                    combined_periods = combined_periods.reset_index(drop=True)
+                    combo_dict['combined_periods_2']= combined_periods
+                    print ("periodsCombined")
+                except KeyError:
+                    pass
+                    print ("no new key")
+    return photo_data
